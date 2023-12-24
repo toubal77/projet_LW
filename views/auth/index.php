f8e69221b9fb6001874eecf9a8e8d0d4ad959b38
toubal zine-eddine <toubalzineddine77@gmail.com>
2023-12-24 11:50:44 +0100

    correct bug redirect login

--- views/auth/index.php
+++ views/auth/index.php
@@ -2,11 +2,11 @@
 
 <h2 style="margin-bottom: 20px; color: #333; font-size: 24px;">Connexion</h2>
 
-<form action="#" method="post">
+<form action="/project_LW/admin/index" method="post">
     <input type="text" name="username" placeholder="Nom d'utilisateur" style="width: 100%; padding: 10px; margin-bottom: 10px; box-sizing: border-box; border: 1px solid #ccc; border-radius: 4px;" required>
     <input type="password" name="password" placeholder="Mot de passe" style="width: 100%; padding: 10px; margin-bottom: 10px; box-sizing: border-box; border: 1px solid #ccc; border-radius: 4px;" required>
-    <button type="submit" style="background-color: #007BFF; color: #fff; padding: 10px; border: none; width: 100%; cursor: pointer; border-radius: 4px; font-size: 16px;">Se Connecter</button>
-    <p style="margin-top: 10px;">Pas encore de compte? <a href="signUp">Inscrivez-vous</a></p>
+    <input type="submit" name="submitForm" value="login"  style="background-color: #007BFF; color: #fff; padding: 10px; border: none; width: 100%; cursor: pointer; border-radius: 4px; font-size: 16px;" >
+    <p style="margin-top: 10px;">Pas encore de compte? <a href="/project_LW/auth/signUp">Inscrivez-vous</a></p>
 </form>
 
 </div>
--- views/auth/signUp.php
+++ views/auth/signUp.php
@@ -2,12 +2,12 @@
 
 <h2 style="margin-bottom: 20px; color: #333; font-size: 24px;">Connexion</h2>
 
-<form action="#" method="post">
+<form action="/project_LW/posts/index" method="post">
     <input type="text" name="username" placeholder="Nom d'utilisateur" style="width: 100%; padding: 10px; margin-bottom: 10px; box-sizing: border-box; border: 1px solid #ccc; border-radius: 4px;" required>
     <input type="email" name="email" placeholder="toubal@gmail.com" style="width: 100%; padding: 10px; margin-bottom: 10px; box-sizing: border-box; border: 1px solid #ccc; border-radius: 4px;" required>
     <input type="password" name="password" placeholder="Mot de passe" style="width: 100%; padding: 10px; margin-bottom: 10px; box-sizing: border-box; border: 1px solid #ccc; border-radius: 4px;" required>
-    <button type="submit" style="background-color: #007BFF; color: #fff; padding: 10px; border: none; width: 100%; cursor: pointer; border-radius: 4px; font-size: 16px;">Se Connecter</button>
-    <p style="margin-top: 10px;">Vous avez un compte? <a href="index">Connecter vous</a></p>
+    <input type="submit" name="submitForm" value="login"  style="background-color: #007BFF; color: #fff; padding: 10px; border: none; width: 100%; cursor: pointer; border-radius: 4px; font-size: 16px;" >
+    <p style="margin-top: 10px;">Vous avez un compte? <a href="/project_LW/auth/index">Connecter vous</a></p>
 </form>
 
 </div>
