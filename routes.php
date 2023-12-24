6cf9bf4dc8112e49e838840ecaf824ced087db2a
toubal zine-eddine <toubalzineddine77@gmail.com>
2023-12-24 11:40:17 +0100

    delete page

--- routes.php
+++ routes.php
@@ -3,9 +3,6 @@
     require_once('controllers/' . $controller . '_controller.php');
 
     switch($controller) {
-      case 'pages':
-        $controller = new PagesController();
-      break;
       case 'posts':
         // we need the model to query the database later in the controller
         require_once('models/post.php');
@@ -27,7 +24,6 @@
 // we're adding an entry for the new controller and its actions
 $controllers = array(
   'auth' => ['index', 'signUp'],
-  'pages' => ['index', 'error'],
   'posts' => ['index', 'create','show'],
   'admin' => ['index', 'show']
 );
