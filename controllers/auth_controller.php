1e09dfd145b5adced770e08da2cddcf01c9cfd36
toubal zine-eddine <toubalzineddine77@gmail.com>
2023-12-25 08:34:03 +0100

    add session

--- controllers/auth_controller.php
+++ controllers/auth_controller.php
@@ -1,12 +1,63 @@
 <?php
+session_start();
   class AuthController {
 
-
     public function index() {
+
+      if(isset($_POST["submitForm"])){
+        $this->utilisateurs = new Utilisateurs();
+        // Handle login form submission
+        if (isset($_POST['email']) && isset($_POST['mot_de_passe'])) {
+            $email = $_POST['email'];
+            $password = $_POST['mot_de_passe'];
+            $user = $this->utilisateurs->loginUser($email, $password);
+    
+            if ($user) {
+              $_SESSION['user'] = $user;
+              $_SESSION['id']= $user['idUtilisateurs'];
+              $_SESSION['username']= $user['nom'];
+              $_SESSION['email']= $user['email'];
+              $_SESSION['role']= $user['role'];
+              if($user['role']='admin'){
+               require_once('views/admin/index.php');
+              }else if($user['role']='user'){
+                require_once('views/posts/index.php');
+              }
+            } else {
+              require_once('views/posts/index.php');
+                echo "error authentication";
+            }
+        } else {
+        }
+      }
       require_once('views/auth/index.php');
     }
 
     public function signUp() {
+      if(isset($_POST["submitForm"])){
+        $this->utilisateurs = new Utilisateurs();
+        // Handle login form submission
+        if (isset($_POST['email']) && isset($_POST['mot_de_passe'])&& isset($_POST['username'])) {
+            $email = $_POST['email'];
+            $password = $_POST['mot_de_passe'];
+            $username = $_POST['username'];
+            $user = $this->utilisateurs->registerUser($email,$username, $password);
+    
+            if ($user) {
+                $_SESSION['user'] = $user;
+                $_SESSION['id']= $user['idUtilisateurs'];
+                $_SESSION['username']= $user['nom'];
+                $_SESSION['email']= $user['email'];
+                $_SESSION['role']= $user['role'];
+                require_once('views/posts/index.php');
+              
+            } else {
+              require_once('views/posts/index.php');
+                echo "error authentication";
+            }
+        } else {
+        }
+      }
       require_once('views/auth/signUp.php');
     }
 
@@ -22,24 +73,6 @@
 
       // Other logic and view rendering
   }
-
-  public function login() {
-    $this->utilisateurs = new Utilisateurs();
-    // Handle login form submission
-    if (isset($_POST['email']) && isset($_POST['mot_de_passe'])) {
-        $email = $_POST['email'];
-        $password = $_POST['mot_de_passe'];
-        $user = $this->utilisateurs->loginUser($email, $password);
-
-        if ($user) {
-            require_once('views/admin/index.php');
-        } else {
-            echo "error authentication";
-        }
-    } else {
-        // Display login form or handle invalid request
-    }
-}
 
 public function signUpUser() {
   $this->utilisateurs = new Utilisateurs();
