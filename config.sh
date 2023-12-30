41e60d85fcbf4f471272ea5cc8a0d64650298888
toubal zine-eddine <toubalzineddine77@gmail.com>
2023-12-30 14:10:41 +0100

    and

--- config.sh
+++ config.sh
@@ -11,7 +11,7 @@ read -e -p $'\nEntrez le nom de la base de données : ' DB_NAME
 CONFIG_FILE="config.php"
 
 SQL_COMMANDS="CREATE TABLE IF NOT EXISTS users (
-  idUtilisateurs int(11) NOT NULL PRIMARY KEY,
+  idUtilisateurs int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   nom varchar(255) NOT NULL,
   role enum('admin','user') NOT NULL DEFAULT 'user',
   email varchar(255) NOT NULL,
