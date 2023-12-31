ffe7f6fb45996fa851f1a64ca7ba68a716b3dd63
toubal zine-eddine <toubalzineddine77@gmail.com>
2023-12-31 12:28:02 +0100

    add file sql & update code SQL script

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : sam. 30 déc. 2023 à 12:10
+-- Généré le : dim. 31 déc. 2023 à 12:25
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -11,9 +11,21 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-DROP TABLE `composant`;
-DROP TABLE `illustrations`;
-DROP TABLE `users`;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `projectLW`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `composant`
+--
 
 CREATE TABLE `composant` (
   `idComposant` int(11) NOT NULL,
@@ -25,6 +37,9 @@ CREATE TABLE `composant` (
 
 -- --------------------------------------------------------
 
+--
+-- Structure de la table `illustrations`
+--
 
 CREATE TABLE `illustrations` (
   `idIllustration` int(11) NOT NULL,
@@ -37,6 +52,9 @@ CREATE TABLE `illustrations` (
 
 -- --------------------------------------------------------
 
+--
+-- Structure de la table `users`
+--
 
 CREATE TABLE `users` (
   `idUtilisateurs` int(11) NOT NULL,
@@ -46,27 +64,58 @@ CREATE TABLE `users` (
   `mot_de_passe` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `users`
+--
 
 INSERT INTO `users` (`idUtilisateurs`, `nom`, `role`, `email`, `mot_de_passe`) VALUES
 (11, 'admin', 'admin', 'admin@gmail.com', 'admin');
 
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `composant`
+--
 ALTER TABLE `composant`
   ADD PRIMARY KEY (`idComposant`);
 
-
+--
+-- Index pour la table `illustrations`
+--
 ALTER TABLE `illustrations`
   ADD PRIMARY KEY (`idIllustration`);
 
+--
+-- Index pour la table `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`idUtilisateurs`);
 
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `composant`
+--
 ALTER TABLE `composant`
-  MODIFY `idComposant` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `idComposant` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
-
+--
+-- AUTO_INCREMENT pour la table `illustrations`
+--
 ALTER TABLE `illustrations`
-  MODIFY `idIllustration` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `idIllustration` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
+--
+-- AUTO_INCREMENT pour la table `users`
+--
 ALTER TABLE `users`
-  MODIFY `idUtilisateurs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `idUtilisateurs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
